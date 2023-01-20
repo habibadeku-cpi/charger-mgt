0ef1015492556715650a10213765c38c131f834a
Habib Adeku <habib.adeku@chargepoint.com>
2023-01-20 10:47:42 +0100

    charger-mgt mfe poc app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const AppHeader = React.lazy(() => import("host/AppHeader"));
+const ProvisioningComponent = React.lazy(() => import("provisn_app/ProvisioningComponent")); 
+const StationMgtComponent = React.lazy(() => import("station_mgt_app/StationMgtComponent"));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppHeader />
+      <div className="app-menu">
+        <div className="app-logo-name">Charger Management</div>
+        <div className="app-menu-list">
+          <ul>
+            <li className="app-menu-list-item">
+              <a href="http://localhost:3002">Provisioning</a>
+            </li>
+            <li className="app-menu-list-item">
+            <a href="http://localhost:3002/station-mgt">Station Management</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="charger-mgt-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProvisioningComponent />} />
+          <Route path="/station-mgt" element={<StationMgtComponent />} />
+        </Routes>
+      </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/remoteTypes.d.ts
@@ -0,0 +1,20 @@
+///<reference types="react" />
+
+declare module 'host/AppHeader' {
+    const AppHeader: React.ComponentType;
+  
+    export default AppHeader;
+}
+
+declare module 'provisn_app/ProvisioningComponent' {
+    const ProvisioningComponent: React.ComponentType;
+  
+    export default ProvisioningComponent;
+}
+
+declare module 'station_mgt_app/StationMgtComponent' {
+    const StationMgtComponent: React.ComponentType;
+  
+    export default StationMgtComponent;
+}
+  
